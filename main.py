9631a34183b7e09d89bd12ce7ad02519859201ba
Saimun Hassan <saimun4u@gmail.com>
2023-04-02 15:05:38 -0400

    Function setup for the game has been completed

--- main.py
+++ main.py
@@ -3,13 +3,26 @@
 
 import random
 
+#Ask for input
 def play():
-    user = input("'r' for rock, 'p' for paper, 's' for scissors")
+    user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
     computer = random.choice(['r', 'p', 's'])
+#Tie logic
+    if user == computer:
+        return 'It\'s a tie'
+#Selecting the winner
+    if is_win(user,computer):
+       return 'You won!'
+    return 'You lost!'
 
-    if user === computer:
-        return 'tie'
-
-
+# Logic for the game
+def is_win(player, opponent):
+    # return true if player wins
+     # r > s, s > p, p > r
+  if (player == 'r' and opponent = 's') or (player == 's' and opponent == 'p') \
+  or (player == 'p' and opponent == 'r'):
+     return True
+     
+print(play())
 
     
